88550275a21dbad4a2fe7871a4bb5b860ea2d6d2
Marcos Kichel <marcos.kichel@gmail.com>
2022-02-11 19:30:25 -0300

    Replace div with Box

--- containers/LiveStats/LiveStats.tsx
+++ containers/LiveStats/LiveStats.tsx
@@ -1,14 +1,15 @@
+import { Box } from '@mui/material';
 import StatsItem from '../../components/StatsItem/StatsItem';
 
 const data = [{ title: 'Total Value Locked' }, { title: 'SOL/scnSOL in Pool' }];
 
 const LiveStats = () => {
   return (
-    <div>
+    <Box sx={{ display: 'flex' }}>
       {data.map((item) => (
         <StatsItem key={item.title} title={item.title} />
       ))}
-    </div>
+    </Box>
   );
 };
 
